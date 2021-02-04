0bd7d33377422e5c2a07456471c751ddffbd0466
WuJi <710977702@qq.com>
2021-02-04 09:52:48 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题 easy

--- main.cpp
+++ main.cpp
@@ -344,19 +344,40 @@ using namespace  std;
 //解释：最大平均数 (12-5-6+50)/4 = 51/4 = 12.75
 //来源：力扣（LeetCode）
 //链接：https://leetcode-cn.com/problems/maximum-average-subarray-i
+//int main(){
+//    vector<int> nums;
+//    int k;
+//    int ans=0;
+//    int s=0;
+//    for(int i=0;i<k;i++){
+//        s+=nums[i];
+//    }
+//    ans=s;
+//    for(int i=k;i<nums.size();i++){
+//        s+=nums[i];
+//        s-=nums[i-k];
+//        ans=max(ans,s);
+//    }
+//    return double(ans)/double(k);
+//}
+
+
+//剑指 Offer 10- II. 青蛙跳台阶问题 easy
+//2021年2月4日09点50分
+//一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+//答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
 int main(){
-    vector<int> nums;
-    int k;
-    int ans=0;
-    int s=0;
-    for(int i=0;i<k;i++){
-        s+=nums[i];
+    int n=4;
+    if(n==0||n==1)
+        return 1;
+    int a=1;
+    int b=1;
+    int c=0;
+    for(int i=1;i<n;i++){
+        a=(a+b)%1000000007;
+        swap(a,b);
     }
-    ans=s;
-    for(int i=k;i<nums.size();i++){
-        s+=nums[i];
-        s-=nums[i-k];
-        ans=max(ans,s);
-    }
-    return double(ans)/double(k);
+    return b;
 }
