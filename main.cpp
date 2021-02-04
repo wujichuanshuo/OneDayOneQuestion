310330075e49e6de52233aac24572d653c51bb51
WuJi <710977702@qq.com>
2021-02-05 01:36:00 +0800

    1208. 尽可能使字符串相等 mid

--- main.cpp
+++ main.cpp
@@ -368,16 +368,49 @@ using namespace  std;
 //答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
 //来源：力扣（LeetCode）
 //链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+//int main(){
+//    int n=4;
+//    if(n==0||n==1)
+//        return 1;
+//    int a=1;
+//    int b=1;
+//    int c=0;
+//    for(int i=1;i<n;i++){
+//        a=(a+b)%1000000007;
+//        swap(a,b);
+//    }
+//    return b;
+//}
+
+
+//1208. 尽可能使字符串相等 mid
+//2021年2月5日1点34分
+//给你两个长度相同的字符串，s 和 t。
+//将 s 中的第 i 个字符变到 t 中的第 i 个字符需要 |s[i] - t[i]| 的开销（开销可能为 0），也就是两个字符的 ASCII 码值的差的绝对值。
+//用于变更字符串的最大预算是 maxCost。在转化字符串时，总开销应当小于等于该预算，这也意味着字符串的转化可能是不完全的。
+//如果你可以将 s 的子字符串转化为它在 t 中对应的子字符串，则返回可以转化的最大长度。
+//如果 s 中没有子字符串可以转化成 t 中对应的子字符串，则返回 0。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/get-equal-substrings-within-budget
 int main(){
-    int n=4;
-    if(n==0||n==1)
-        return 1;
-    int a=1;
-    int b=1;
-    int c=0;
-    for(int i=1;i<n;i++){
-        a=(a+b)%1000000007;
-        swap(a,b);
+    string s="abcd";
+    string t="cdef";
+    int maxCost=3;
+    int left=0;
+    int right=0;
+    int ans=0;
+    int tmp=0;
+    while (right<s.size()){
+        if(tmp>maxCost){
+            tmp-=abs(s[left]-t[left]);
+            ans=max(ans,right-left-1);
+            left++;
+        }
+        else{
+            tmp+=abs(s[right]-t[right]);
+            right++;
+        };
     }
-    return b;
+    ans = max(ans, tmp <= maxCost ? right - left : right - left - 1);
+    return ans;
 }
